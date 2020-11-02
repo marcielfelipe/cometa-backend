ab60a8991cf414e1db71ed24d1dbc9232c1a51c4
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2020-11-02 08:51:49 -0300

    status Concluído

--- cometa/src/routes.js
+++ cometa/src/routes.js
@@ -12,7 +12,8 @@ const routes=Router();
 routes.post("/service",ServiceController.create);
 routes.get("/",ServiceController.index);
 routes.put("/",ServiceController.edit);
-routes.get("/service",ServiceController.getOpen);
+routes.get("/service/open",ServiceController.getOpen);
+routes.get("/service/close",ServiceController.getOpen);
 
 
 routes.post("/file",multer(multerConfig).single('file'),FileController.create);
